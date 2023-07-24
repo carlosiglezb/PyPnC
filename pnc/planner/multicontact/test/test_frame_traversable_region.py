b6667725140ae6c8b8e1da7142cedcb589efc317
Carlos Gonzalez <carlos.gonzalez@utexas.edu>
2023-07-24 17:50:47 -0500

    added navy door test environment

--- pnc/planner/multicontact/test/test_frame_traversable_region.py
+++ pnc/planner/multicontact/test/test_frame_traversable_region.py
@@ -1,4 +1,6 @@
 import unittest
+
+import util.util
 from pnc.planner.multicontact.frame_traversable_region import FrameTraversableRegion
 
 import os
@@ -61,6 +63,14 @@ class TestFrameTraversableRegion(unittest.TestCase):
         floating_base = np.array([0., 0., 0.74, 0., 0., 0., 1.])
         return np.concatenate((floating_base, q0))
 
+    def get_navy_door_default_initial_pose(self):
+        # rotates, then translates
+        pos = np.array([0.3, 0., 0.])
+        R = util.util.euler_to_rot([0., 0., np.pi/2.])
+        quat = util.util.rot_to_quat(R)
+        return np.concatenate((pos, quat))
+
+
     def get_sample_collision_free_boxes(self):
         L_lf = np.array([
             [-0.1, -0.1, 0.0],  # prevent leg-crossing
@@ -103,6 +113,53 @@ class TestFrameTraversableRegion(unittest.TestCase):
             [0.1, 0.1, 1.3],  # prevent leg-crossing
             [0.4, 0.0, 1.3],  # prevent leg-crossing
             [0.5, 0.1, 1.3]  # prevent leg-crossing
+        ])
+        box_llim=[L_lf, L_rf, L_lh, L_rh]
+        box_ulim=[U_lf, U_rf, U_lh, U_rh]
+        return box_llim, box_ulim
+
+    def get_navy_door_collision_free_boxes(self):
+        L_lf = np.array([
+            [-0.1, 0.0, 0.0],  # prevent leg-crossing
+            [-0.1, 0.0, 0.4],  # prevent leg-crossing
+            [0.4, 0.0, 0.0]  # prevent leg-crossing
+        ])
+        L_rf = np.array([
+            [-0.2, -0.4, 0.0],  # prevent leg-crossing
+            [-0.1, -0.35, 0.4],  # prevent leg-crossing
+            [0.4, -0.4, 0.0]  # prevent leg-crossing
+        ])
+        L_lh = np.array([
+            [-0.2, 0.0, 0.7],  # prevent leg-crossing
+            [-0.1, 0.0, 0.7],  # prevent leg-crossing
+            [0.4, 0.0, 0.7]  # prevent leg-crossing
+        ])
+        L_rh = np.array([
+            [-0.2, -0.4, 0.7],  # prevent leg-crossing
+            [-0.1, -0.4, 0.7],  # prevent leg-crossing
+            [0.4, -0.4, 0.7]  # prevent leg-crossing
+        ])
+
+        # upper bounds of the safe boxes
+        U_lf = np.array([
+            [0.25, 0.35, 0.6],  # z stops at kin. limit
+            [0.6, 0.4, 0.6],  # x stops at kin. limit
+            [0.6, 0.4, 0.6]  # x stops at kin. limit
+        ])
+        U_rf = np.array([
+            [0.25, 0.0, 0.6],  # prevent leg-crossing
+            [0.6, 0.0, 0.6],  # prevent leg-crossing
+            [0.6, 0.0, 0.6]  # prevent leg-crossing
+        ])
+        U_lh = np.array([
+            [0.25, 0.4, 1.3],  # prevent leg-crossing
+            [0.6, 0.4, 1.3],  # prevent leg-crossing
+            [0.6, 0.4, 1.3]  # prevent leg-crossing
+        ])
+        U_rh = np.array([
+            [0.25, 0.0, 1.3],  # prevent leg-crossing
+            [0.6, 0.0, 1.3],  # prevent leg-crossing
+            [0.6, 0.0, 1.3]  # prevent leg-crossing
         ])
         box_llim=[L_lf, L_rf, L_lh, L_rh]
         box_ulim=[U_lf, U_rf, U_lh, U_rh]
@@ -182,6 +239,49 @@ class TestFrameTraversableRegion(unittest.TestCase):
         box_llim, box_ulim = self.get_sample_collision_free_boxes()
         test_region.load_collision_free_boxes(box_llim[1], box_ulim[1])
 
+    def test_visualize_navy_door_paths(self):
+        # load robot
+        rob_model, rob_collision_model, rob_visual_model = pin.buildModelsFromUrdf(
+            cwd + "/robot_model/draco3/draco3_gripper_mesh_updated.urdf",
+            cwd + "/robot_model/draco3", pin.JointModelFreeFlyer())
+        visualizer = MeshcatVisualizer(rob_model, rob_collision_model, rob_visual_model)
+
+        try:
+            visualizer.initViewer(open=True)
+            visualizer.viewer.wait()
+        except ImportError as err:
+            print(
+                "Error while initializing the viewer. It seems you should install Python meshcat"
+            )
+            print(err)
+            sys.exit(0)
+        visualizer.loadViewerModel(rootNodeName="draco3")
+        vis_q = self.get_draco3_default_initial_pose()
+        visualizer.display(vis_q)
+
+        # load door to visualizer
+        door_model, door_collision_model, door_visual_model = pin.buildModelsFromUrdf(
+            cwd + "/robot_model/ground/navy_door.urdf",
+            cwd + "/robot_model/ground", pin.JointModelFreeFlyer())
+        door_vis = MeshcatVisualizer(door_model, door_collision_model, door_visual_model)
+        door_vis.initViewer(visualizer.viewer)
+        door_vis.loadViewerModel(rootNodeName="door")
+        door_vis_q = self.get_navy_door_default_initial_pose()
+        door_vis.display(door_vis_q)
+
+        test_region = FrameTraversableRegion(self.frame_name,
+                              self.frame_stl_path, self.poly_halfspace_path,
+                              visualizer=visualizer)
+        self.assertEqual(True, True)
+
+        # move to standing height
+        standing_pos = vis_q[:3]
+        test_region.update_origin_pose(standing_pos)
+        self.assertEqual(True, True)
+
+        # collision-free boxes
+        box_llim, box_ulim = self.get_navy_door_collision_free_boxes()
+        test_region.load_collision_free_boxes(box_llim[1], box_ulim[1])
 
 if __name__ == '__main__':
     unittest.main()
