50f0dbb93a057a7ed91b6066f2cc0fe5885117a2
Carlos Gonzalez <carlos.gonzalez@utexas.edu>
2024-01-11 19:20:37 -0600

    added simple test for box interpolator

---
+++ pnc/planner/multicontact/test/test_fpp_tools.py
@@ -0,0 +1,30 @@
+import unittest
+
+import numpy as np
+from pnc.planner.multicontact.fastpathplanning.fastpathplanning import unassigned_box_seq_interpolator
+
+
+class TestFPPTools(unittest.TestCase):
+    def test_unassigned_box_seq_interp_uneven(self):
+        test_frame = 'torso'
+
+        # generate sequence with uneven number of undefined boxes
+        des_box_seq = [{test_frame: [0]},
+                       {test_frame: [1]},
+                       {test_frame: [1]}]
+        box_seq = [{test_frame: [0]},
+                   {test_frame: [np.nan]},
+                   {test_frame: [np.nan]}]
+        last_box_seq = {test_frame: [0, 1, 2]}
+
+        # call method
+        unassigned_box_seq_interpolator(box_seq, last_box_seq, test_frame)
+
+        # compare against expected result
+        for bs, des_bs in zip(box_seq, des_box_seq):
+            for box, des_box in zip(bs[test_frame], des_bs[test_frame]):
+                self.assertEqual(box, des_box)
+
+
+if __name__ == '__main__':
+    unittest.main()
