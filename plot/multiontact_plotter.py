aaaea336b0cd6911899fa9458d31882b830ce2c8
Carlos Gonzalez <carlos.gonzalez@utexas.edu>
2024-09-22 18:22:25 -0500

    fixed plotting dimensions to avoid plotting extra zeros

--- plot/multiontact_plotter.py
+++ plot/multiontact_plotter.py
@@ -19,22 +19,26 @@ class MulticontactPlotter:
         xr_dim = len(self.lleg_joint_ids)
         ur_dim = len(self.lleg_joint_ids)
 
-        xs_l_reduced = np.zeros((sum(horizon_lst) - 1, xr_dim))
-        us_l_reduced = np.zeros((sum(horizon_lst) - 1, ur_dim))
-        xs_r_reduced = np.zeros((sum(horizon_lst) - 1, xr_dim))
-        us_r_reduced = np.zeros((sum(horizon_lst) - 1, ur_dim))
-        time = np.zeros(sum(horizon_lst) - 1)
-        phase = np.zeros(sum(horizon_lst) - 1, dtype=int)
+        xs_l_reduced = np.zeros((sum(horizon_lst) - len(fddp) + 1, xr_dim))
+        us_l_reduced = np.zeros((sum(horizon_lst) - len(fddp), ur_dim))
+        xs_r_reduced = np.zeros((sum(horizon_lst) - len(fddp) + 1, xr_dim))
+        us_r_reduced = np.zeros((sum(horizon_lst) - len(fddp), ur_dim))
+        time = np.zeros(sum(horizon_lst) - len(fddp) + 1)
+        phase = np.zeros(sum(horizon_lst) - len(fddp) + 1, dtype=int)
         curr_idx = 0
         for (it_num, it) in enumerate(fddp):
-            next_idx = curr_idx + horizon_lst[it_num] + 1   # add terminal impulse models
+            if it_num == (len(fddp) - 1):
+                next_idx = curr_idx + horizon_lst[it_num]  # last phase does not have impulse model
+                time[curr_idx:next_idx] = np.linspace(it_num * T, (it_num + 1) * T, horizon_lst[it_num])
+            else:
+                next_idx = curr_idx + horizon_lst[it_num] + 1   # add terminal impulse models
+                time[curr_idx:next_idx] = np.linspace(it_num * T, (it_num + 1) * T, horizon_lst[it_num] + 1)
 
             log = it.getCallbacks()[0]
-            xs_l_reduced[curr_idx:next_idx, :] = np.array(log.xs)[:, lleg_jids]
+            xs_l_reduced[curr_idx:next_idx - 1, :] = np.array(log.xs)[:-1, lleg_jids]
             us_l_reduced[curr_idx:next_idx - 1, :] = np.array(log.us)[:, lleg_jids]
-            xs_r_reduced[curr_idx:next_idx, :] = np.array(log.xs)[:, rleg_jids]
+            xs_r_reduced[curr_idx:next_idx - 1, :] = np.array(log.xs)[:-1, rleg_jids]
             us_r_reduced[curr_idx:next_idx - 1, :] = np.array(log.us)[:, rleg_jids]
-            time[curr_idx:next_idx] = np.linspace(it_num * T, (it_num+1) * T, horizon_lst[it_num] + 1)
             phase[curr_idx:next_idx] = int(it_num)
             curr_idx += horizon_lst[it_num] - 1
 
@@ -45,7 +49,7 @@ class MulticontactPlotter:
             xs_names[jn_i] = 'q_' + jn
             us_names[jn_i] = 'u_' + jn
         signals_names = [xs_names, us_names]
-        plot_multiple_state_traj(time[:-1], [xs_l_reduced[:-1, :], us_l_reduced[:-1, :]],
+        plot_multiple_state_traj(time[:-1], [xs_l_reduced[:-1, :], us_l_reduced[:, :]],
                                  phase, ax_labels=signals_names)
 
         # create names of signals and plot right leg joints
@@ -53,19 +57,19 @@ class MulticontactPlotter:
             xs_names[jn_i] = 'q_' + jn
             us_names[jn_i] = 'u_' + jn
         signals_names = [xs_names, us_names]
-        plot_multiple_state_traj(time[:-1], [xs_r_reduced[:-1, :], us_r_reduced[:-1, :]],
+        plot_multiple_state_traj(time[:-1], [xs_r_reduced[:-1, :], us_r_reduced[:, :]],
                                  phase, ax_labels=signals_names)
 
     def _get_lleg_joint_ids(self):
         lleg_j_ids = []
         robot_model = self._robot_planner.robot_model
         for jname in self._robot_planner.lleg_jnames:
-            lleg_j_ids.append(list(robot_model.names).index(jname) - 2 + 7)
+            lleg_j_ids.append(robot_model.idx_qs[list(robot_model.names).index(jname)])
         return lleg_j_ids
 
     def _get_rleg_joint_ids(self):
         rleg_j_ids = []
         robot_model = self._robot_planner.robot_model
         for jname in self._robot_planner.rleg_jnames:
-           rleg_j_ids.append(list(robot_model.names).index(jname) - 2 + 7)
+           rleg_j_ids.append(robot_model.idx_qs[list(robot_model.names).index(jname)])
         return rleg_j_ids
