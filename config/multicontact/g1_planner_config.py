2e5a16981d915ed615dd5b15ec6e9ce12fc11740
Carlos Gonzalez <carlos.gonzalez@utexas.edu>
2025-07-18 02:16:00 -0500

    tuning for G1 stepping over knee knocker (seq0)

--- config/multicontact/g1_planner_config.py
+++ config/multicontact/g1_planner_config.py
@@ -5,26 +5,27 @@ N_V = 33  # dimension of generalized velocities
 N_U = 27  # dimension of control inputs
 
 class MultiContactDoorConfig(PlannerConfig):
-    W_RIGID_LINK = [5., 0., 0.]
+    W_RIGID_LINK = [10., 0., 0.]
     # W_RIGID_LINK_SINGLE_STEP = [5., 0., 0.]
     # W_RIGID_LINK_STEP_ON_DOOR = [1000., 0., 0.]
 
-    ALPHA = [0.5, 0.1, 0.01]
+    ALPHA = [1, 0, 0.1]    # seq 0: single hand, step through door
+    # ALPHA = [0.5, 0.1, 0.01]
     FOOT_SIZE = [0.15, 0.08]  # [length, width]
     N_HORIZON_LST = [180, 200, 220, 200, 200]
 
     # Note: contact seq 1 tested with:
     # 'torso', np.array([2.5, 3.5, 1.5] + [0.5, 0.5, 0.001])
     WBC_FRAME_TRACKING_GAINS = {
-            'torso': np.array([2.5, 1.5, 1.0] + [0.5, 0.5, 0.01]),  # (lin, ang)
-            'feet': np.array([12.] * 3 + [0.00001] * 3),  # (lin, ang)
+            'torso': np.array([3, 3, 1.0] + [0.1, 0.1, 0.01]),  # (lin, ang)
+            'feet': np.array([8.] * 3 + [0.00001] * 3),  # (lin, ang)
             'L_knee': np.array([8.] * 3 + [0.00001] * 3),
-            'R_knee': np.array([8.] * 3 + [0.00001] * 3),
-            'LH': np.array([2.] * 3 + [0.00001] * 3),
-            'RH': np.array([2.] * 3 + [0.00001] * 3)
+            'R_knee': np.array([10.] * 3 + [0.00001] * 3),
+            'LH': np.array([4.] * 3 + [0.00001] * 3),
+            'RH': np.array([4.] * 3 + [0.00001] * 3)
         }
     WBC_WEIGHTED_COSTS = {
-        'xReg': np.array([0.1] * 3 + [10.0] * 3 + [2.] * (N_V - 6) + [4.] * N_V),
+        'xReg': np.array([0.1] * 3 + [5.0] * 3 + [2.] * (N_V - 6) + [0.2] * 6 + [4.] * (N_V - 6)),
         'uReg': np.array([0.5] * N_U),
     }
 
--- config/multicontact/planner_config.py
+++ config/multicontact/planner_config.py
@@ -9,20 +9,20 @@ class PlannerConfig(ABC):
     WBC_FRAME_TRACKING_GAINS : dict[str: np.ndarray] = {}  # gains for the whole-body planner frame tracking tasks
     WBC_WEIGHTED_COSTS : dict[str: np.ndarray] = {}  # costs for the whole-body planner frame tracking tasks
     WBC_COST_WEIGHTS = {
-        'friction': 1e1,
-        'frame_goal': 6e4,
+        'friction': 1e2,
+        'frame_goal': 1e3,
         'xReg': 5e-3,
         'uReg': 1e-4,
         'xBounds': 5e4,
     }
     WBC_FINAL_COST_WEIGHTS = {
-        'friction': 1e1,
-        'frame_goal': 5e6,
+        'friction': 1e2,
+        'frame_goal': 4e3,
         'xReg': 5e-5,
         'uReg': 1e-4,
-        'xBounds': 1000,
+        'xBounds': 5e4,
     }
     WBC_IMPULSE_COST_WEIGHTS = {
-        'frame_goal': 100,
+        'frame_goal': 1000,
         'xReg': 5e-2,
     }
