1dba1706bf83b41fb74ac9b86737ff4092815b2e
Carlos Gonzalez <carlos.gonzalez@utexas.edu>
2025-08-07 18:04:57 -0500

    plot CoM distance to polytope surface on G1 step over knee knocker motion

--- pnc/planner/multicontact/kin_feasibility/test/test_g1_stabilipy.py
+++ pnc/planner/multicontact/kin_feasibility/test/test_g1_stabilipy.py
@@ -3,6 +3,8 @@ import unittest
 import numpy as np
 import os, sys
 
+from util.polytope_math import get_closest_distance_to_polytope_surface
+
 cwd = os.getcwd()
 
 from meshcat.geometry import Sphere
@@ -16,7 +18,7 @@ import pinocchio as pin
 import external_source.stabilipy.stabilipy as stab
 import pickle
 import plot.meshcat_utils as vis_tools
-
+import matplotlib.pyplot as plt
 
 @staticmethod
 def get_contact_poses_from_file(filename: str) -> list[np.ndarray]:
@@ -25,7 +27,7 @@ def get_contact_poses_from_file(filename: str) -> list[np.ndarray]:
         while True:
             try:
                 d = pickle.load(file)
-                q_init_lst.append(d['joint_pos'][0, :])
+                q_init_lst.append(np.array(d['joint_pos'][0]))
             except EOFError:
                 break
 
@@ -48,10 +50,11 @@ def get_all_poses_from_file(filename: str) -> list[np.ndarray]:
 class TestStabilipy(unittest.TestCase):
     def test_stabilipy_meshcat_visualization(self):
         b_plot_final = False
+        dist_in_lst, dist_out_lst = [], []
 
         # Specify location of urdf files
-        robot_name = "g1"
-        urdf_file = cwd + "/robot_model/g1_description/g1.urdf"
+        robot_name = "g1_29dof_lock_waist"
+        urdf_file = cwd + "/robot_model/g1_description/g1_29dof_lock_waist_modified.urdf"
         package_dir = cwd + "/robot_model/g1_description"
 
         # Create robot system
@@ -75,7 +78,7 @@ class TestStabilipy(unittest.TestCase):
         vis_q = pin.neutral(model)
 
         # get list of configurations throughout multiple contacts
-        cfree_soln_file = cwd + '/experiment_data/g1_knee_knocker_cs_0.pkl'
+        cfree_soln_file = cwd + '/experiment_data/g1_sca_step_over_knee_knocker.pkl'
         q_at_contact = get_contact_poses_from_file(cfree_soln_file)
 
         # robot-specific default parameters
@@ -171,16 +174,35 @@ class TestStabilipy(unittest.TestCase):
             com_pos = data.com[0]
             com_pos_proj = com_pos[0], com_pos[1], 0.
 
+            # distance from outer polyhedron to CoM
+            p_o = HPolyhedron(polyhedron.outer.halfspaces[:,:3], -polyhedron.outer.halfspaces[:, -1])
+            dist_out, _ = p_o.Projection(com_pos)
+
+            # distance from inner polyhedron to CoM
+            p_i = HPolyhedron(polyhedron.inner.equations[:, :3], -polyhedron.inner.equations[:, -1])
+            dist_in, _ = p_i.Projection(com_pos)
+
+            # make distance negative if CoM is outside the polytope
+            if not p_i.PointInSet(com_pos_proj):
+                dist_in *=-1
+
+            # make distance negative if CoM is outside the polytope
+            if not p_o.PointInSet(com_pos_proj):
+                dist_out *=-1
+            # dist = get_closest_distance_to_polytope_surface(com_pos, p_o.A(), p_o.b())
+            dist_in_lst.append(dist_in)
+            dist_out_lst.append(dist_out)
+
             #
             # visualize in meshcat
             #
+            # get meshes of stability polytope
+            p_i_mcat = scipy_hull_to_meshcat(polyhedron.inner)
+            p_o_mcat = pydrake_geom_to_meshcat(p_o)
             # --- stability polytope
-            p_inner = scipy_hull_to_meshcat(polyhedron.inner)
-            viz.viewer[f"{robot_name}/stability/inner"].set_object(p_inner, meshcat_iris_obj())
+            viz.viewer[f"{robot_name}/stability/inner"].set_object(p_i_mcat, meshcat_iris_obj())
             viz.viewer[f"{robot_name}/stability/inner"].set_transform(tf.translation_matrix([0., 0., com_pos[2]]))
-            p_o = HPolyhedron(polyhedron.outer.halfspaces[:,:3], -polyhedron.outer.halfspaces[:, -1])
-            p_outer = pydrake_geom_to_meshcat(p_o)
-            viz.viewer[f"{robot_name}/stability/outer"].set_object(p_outer, meshcat_domain_obj())
+            viz.viewer[f"{robot_name}/stability/outer"].set_object(p_o_mcat, meshcat_domain_obj())
             viz.viewer[f"{robot_name}/stability/outer"].set_transform(tf.translation_matrix([0., 0., com_pos[2]]))
             # --- contacts
             c_obj = Sphere(0.01)
@@ -206,12 +228,12 @@ class TestStabilipy(unittest.TestCase):
         b_plot_final = False
 
         # Specify location of urdf files
-        robot_name = "g1"
-        urdf_file = cwd + "/robot_model/g1_description/g1.urdf"
+        robot_name = "g1_29dof_lock_waist"
+        urdf_file = cwd + "/robot_model/g1_description/g1_29dof_lock_waist_modified.urdf"
         package_dir = cwd + "/robot_model/g1_description"
 
         # get list of configurations throughout multiple contacts
-        cfree_soln_file = cwd + '/experiment_data/g1_knee_knocker_cs_0.pkl'
+        cfree_soln_file = cwd + '/experiment_data/g1_sca_step_over_knee_knocker.pkl'
         q_all = get_all_poses_from_file(cfree_soln_file)
 
         # Create robot system
@@ -223,9 +245,11 @@ class TestStabilipy(unittest.TestCase):
         # Create Meshcat Animation
         save_freq = 1
         T = 3
-        N_horizon_lst = [180, 200, 220, 200, 200] # TODO get from saved data?
+        N_horizon_lst = [len(q_all[k])-1 for k in range(len(q_all))]
         display = vis_tools.MeshcatPinocchioAnimation(model, collision_model, visual_model,
                           data, vis_data, col_data, ctrl_freq=np.average(N_horizon_lst)/T, save_freq=save_freq)
+
+        dist_in_lst, dist_out_lst, time_lst = [], [], []
 
         # robot-specific default parameters
         ankle_heel_dist = 0.06
@@ -241,16 +265,16 @@ class TestStabilipy(unittest.TestCase):
         com_obj = Sphere(0.02)
 
         contacts_seq_lst = [['left_ankle_roll_link', 'right_ankle_roll_link'],
-                            ['right_ankle_roll_link', 'left_palm_link'],
+                            ['right_ankle_roll_link', 'left_rubber_hand'],
                             ['left_ankle_roll_link', 'right_ankle_roll_link'],
-                            ['left_ankle_roll_link', 'right_palm_link'],
+                            ['left_ankle_roll_link', 'right_rubber_hand'],
                             ['left_ankle_roll_link', 'right_ankle_roll_link']]
         # visualize entire motion while super-impossing stability regions after each new contact
         zero_qd = np.zeros((model.nv))
         display.start_animation()
         for (n, N) in enumerate(N_horizon_lst):
             # at each new contact sequence, compute stability region
-            pin.forwardKinematics(model, data, q_all[n][0], zero_qd)
+            pin.forwardKinematics(model, data, np.array(q_all[n][0]), zero_qd)
 
             # set up stabilipy problem
             robot_mass = sum([inertia.mass for inertia in model.inertias])
@@ -275,7 +299,7 @@ class TestStabilipy(unittest.TestCase):
                     # left-back
                     pos.append(ee_pos + np.array([[-ankle_heel_dist], [half_foot_width], [foot_height]]))
                     normals.append(np.array([[0.], [0.], [1.]]))    # feet are flat on ground
-                elif lnk == 'left_palm_link':
+                elif lnk == 'left_rubber_hand':
                     # top-front
                     pos.append(ee_pos + np.array([[hand_box_side], [0.], [hand_box_side]]))
                     normals.append(np.array([[0.], [-1], [0.]]))    # lhand on door side
@@ -288,7 +312,7 @@ class TestStabilipy(unittest.TestCase):
                     # top-back
                     pos.append(ee_pos + np.array([[-hand_box_side], [0.], [hand_box_side]]))
                     normals.append(np.array([[0.], [-1.], [0.]]))    # lhand on door side
-                elif lnk == 'right_palm_link':
+                elif lnk == 'right_rubber_hand':
                     # top-front
                     pos.append(ee_pos + np.array([[hand_box_side], [0.], [hand_box_side]]))
                     normals.append(np.array([[0.], [1], [0.]]))    # rhand on door side
@@ -321,17 +345,19 @@ class TestStabilipy(unittest.TestCase):
             polyhedron.compute(stab.Mode.iteration, epsilon=2e-3, maxIter=10, solver='qhull',
                                record_anim=False, plot_init=False,
                                plot_step=False, plot_final=b_plot_final)
+
             #
             # visualize in meshcat
             #
             # --- stability polytope
             p_in_name = f"{robot_name}/stability/inner/{n}"
             p_out_name = f"{robot_name}/stability/outer/{n}"
-            p_inner = scipy_hull_to_meshcat(polyhedron.inner)
+            p_inner_mcat = scipy_hull_to_meshcat(polyhedron.inner)
             p_o = HPolyhedron(polyhedron.outer.halfspaces[:, :3], -polyhedron.outer.halfspaces[:, -1])
-            p_outer = pydrake_geom_to_meshcat(p_o)
-            display.add_shape(p_in_name, p_inner, meshcat_iris_obj())
-            display.add_shape(p_out_name, p_outer, meshcat_domain_obj())
+            p_outer_mcat = pydrake_geom_to_meshcat(p_o)
+            p_i = HPolyhedron(polyhedron.inner.equations[:, :3], -polyhedron.inner.equations[:, -1])
+            display.add_shape(p_in_name, p_inner_mcat, meshcat_iris_obj())
+            display.add_shape(p_out_name, p_outer_mcat, meshcat_domain_obj())
             # --- contacts
             for i in range(len(contacts)):
                 c_name = f"{robot_name}/contacts/{i}"
@@ -342,12 +368,13 @@ class TestStabilipy(unittest.TestCase):
 
             # visualize configurations in between contacts
             for k in range(N):
-                display.animate_frame(q_all[n][k])
+                display.animate_frame(np.array(q_all[n][k]))
 
                 # visualize CoM
-                pin.centerOfMass(model, data, q_all[n][k], zero_qd)
+                pin.centerOfMass(model, data, np.array(q_all[n][k]), zero_qd)
                 com_pos = data.com[0]
                 com_pos_proj = com_pos[0], com_pos[1], 0.
+
                 # update meshcat frames
                 display.animate_single_shape(p_in_name, tf.translation_matrix([0., 0., com_pos[2]]))
                 display.animate_single_shape(p_out_name, tf.translation_matrix([0., 0., com_pos[2]]))
@@ -361,11 +388,47 @@ class TestStabilipy(unittest.TestCase):
                 display.animate_single_shape(com_proj_name, tf.translation_matrix(com_pos_proj))
                 display.animation_step()
 
+                # distance from outer polyhedron to CoM
+                # dist_out, _ = p_o.Projection(com_pos_proj)
+                dist_out = get_closest_distance_to_polytope_surface(com_pos_proj, p_o.A(), p_o.b())
+
+                # distance from inner polyhedron to CoM
+                # dist_in, _ = p_i.Projection(com_pos_proj)
+                dist_in = get_closest_distance_to_polytope_surface(com_pos_proj, p_i.A(), p_i.b())
+
+                # make distance negative if CoM is outside the polytope
+                if not p_i.PointInSet(com_pos_proj):
+                    dist_in *= -1
+
+                # make distance negative if CoM is outside the polytope
+                if not p_o.PointInSet(com_pos_proj):
+                    dist_out *= -1
+                # dist = get_closest_distance_to_polytope_surface(com_pos, p_o.A(), p_o.b())
+
+                dist_in_lst.append(dist_in)
+                dist_out_lst.append(dist_out)
+                # create list of time steps
+                time_lst.append(T * n + k * T/N)
+
+                # print progress
+                if k % 20 == 0:
+                    print(f"Finished contact phase {n}, step {k}")
                 # check that the current CoM is statically stable
                 self.assertEqual(True, True)
 
         display.finish_animation()
 
+        #
+        # plot distances
+        #
+        plt.figure(figsize=(8, 4))
+        plt.plot(time_lst, dist_in_lst, label='Distance to inner')
+        plt.plot(time_lst, dist_out_lst, label='Distance to outer')
+        plt.ylabel('Distance [m]', fontsize=12)
+        plt.xlabel('Time [s]', fontsize=12)
+        plt.grid()
+        plt.legend(fontsize=12)
+        plt.show()
 
 if __name__ == '__main__':
     unittest.main()
