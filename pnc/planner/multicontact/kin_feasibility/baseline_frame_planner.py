149ddfaf29b9e17d52badd690c177b80f4659b67
Carlos Gonzalez <carlos.gonzalez@utexas.edu>
2025-12-05 00:03:39 -0600

    fixed linear interpolation for when motion/fixed frames are not defined

--- pnc/planner/multicontact/kin_feasibility/baseline_frame_planner.py
+++ pnc/planner/multicontact/kin_feasibility/baseline_frame_planner.py
@@ -13,11 +13,13 @@ class BaselineFramePlanner:
                  plan_to_model_ids: dict[str: int],
                  motion_frames_seq: MotionFrameSequencer,
                  fixed_frames: list[list[str]],
+                 interpolation_time: float=3.0,
                  interpolation:str = "constant",):
         self.robot_data = robot_data
         self.plan_to_model_ids = plan_to_model_ids
         self.all_frame_targets = self.create_frame_targets_dict(motion_frames_seq, fixed_frames)
         self.interpolation = interpolation
+        self.T = interpolation_time
 
     def create_frame_targets_dict(self,
                                   motion_frames_seq: MotionFrameSequencer,
@@ -51,18 +53,39 @@ class BaselineFramePlanner:
             all_frame_targets.append(current_frame_targets)
             phase += 1
 
+        # fill in any missing target frames
+        # loop through each frame backwards in time and record last known position and time
+        for fr_name in frame_names_lst:
+            last_known_pos = None
+            missing_segments = 0
+            for phase in reversed(range(len(all_frame_targets))):
+                if fr_name in all_frame_targets[phase].keys():
+                    if missing_segments > 0 and last_known_pos is not None:
+                        segment_inc = (last_known_pos - all_frame_targets[phase][fr_name]) / (missing_segments+1)
+                        # fill in missing segments with intermediate values
+                        for fill_phase in range(phase + 1, phase + 1 + missing_segments):
+                            all_frame_targets[fill_phase][fr_name] = all_frame_targets[fill_phase-1][fr_name] + segment_inc
+                        missing_segments = 0
+                    last_known_pos = all_frame_targets[phase][fr_name]
+                else:
+                    missing_segments+=1
+
         return all_frame_targets
 
     def get_phase_targets(self, phase: int) -> dict[str, np.array]:
         return self.all_frame_targets[phase]
 
     def get_linear_targets(self, phase: int,
-                           time: float,
-                           final_t: float) -> dict[str, np.array]:
-        s = np.clip(time / final_t, 0.0, 1.0)
+                           time: float) -> dict[str, np.array]:
+        start_t = phase * self.T
+        s = np.clip((time - start_t) / self.T, 0.0, 1.0)
 
         current_phase_targets = self.get_phase_targets(phase)
-        next_phase_targets = self.get_phase_targets(phase + 1)
+        # clip to last phase
+        if phase < len(self.all_frame_targets) - 1:
+            next_phase_targets = self.get_phase_targets(phase + 1)
+        else:
+            next_phase_targets = current_phase_targets
 
         # generate intermediate frames (exclude previous, include final)
         interp_targets = {}
