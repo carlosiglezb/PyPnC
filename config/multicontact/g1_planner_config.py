8960f2d40118c0f9b4fefd89cba0368e1541890d
Carlos Gonzalez <carlos.gonzalez@utexas.edu>
2025-12-18 04:38:14 -0600

    tuning for door demos

--- config/multicontact/g1_planner_config.py
+++ config/multicontact/g1_planner_config.py
@@ -42,14 +42,14 @@ class MultiContactDoorConfig(PlannerConfig):
     FOOT_SIZE = [0.15, 0.08]  # [length, width]
 
     # ----- seq 0 (step over, on_balanced, on)
-    WBC_FRAME_TRACKING_GAINS = {
-            'torso': np.array([1.0, 1.0, 0.5] + [0.5, 0.5, 0.01]),  # (lin, ang)
-            'feet': np.array([8.] * 3 + [0.001] * 3),  # (lin, ang)
-            'L_knee': np.array([4.] * 3 + [0.00001] * 3),
-            'R_knee': np.array([4.] * 3 + [0.00001] * 3),
-            'LH': np.array([4.] * 3 + [0.00001] * 3),
-            'RH': np.array([4.] * 3 + [0.00001] * 3)
-        }
+    # WBC_FRAME_TRACKING_GAINS = {
+    #         'torso': np.array([1.0, 1.0, 1.0] + [0.5, 0.5, 0.001]),  # (lin, ang)
+    #         'feet': np.array([8.] * 3 + [0.001] * 3),  # (lin, ang)
+    #         'L_knee': np.array([0.5] * 3 + [0.00001] * 3),
+    #         'R_knee': np.array([0.5] * 3 + [0.00001] * 3),
+    #         'LH': np.array([4.] * 3 + [0.00001] * 3),
+    #         'RH': np.array([4.] * 3 + [0.00001] * 3)
+    #     }
     # ----- seq 1 (step on)
     # WBC_FRAME_TRACKING_GAINS = {
     #         'torso': np.array([0.5, 0.5, 0.1] + [0.2, 0.2, 0.001]),
@@ -60,17 +60,17 @@ class MultiContactDoorConfig(PlannerConfig):
     #         'RH': np.array([4.] * 3 + [0.00001] * 3)
     #     }
     # ----- seq 2 (step on balanced)
-    # WBC_FRAME_TRACKING_GAINS = {
-    #         'torso': np.array([2.5, 3.5, 2.5] + [0.5, 0.5, 0.001]),
-    #         'feet': np.array([8.] * 3 + [0.00001] * 3),  # (lin, ang)
-    #         'L_knee': np.array([3.] * 3 + [0.00001] * 3),
-    #         'R_knee': np.array([3.] * 3 + [0.00001] * 3),
-    #         'LH': np.array([4.] * 3 + [0.00001] * 3),
-    #         'RH': np.array([4.] * 3 + [0.00001] * 3)
-    #     }
+    WBC_FRAME_TRACKING_GAINS = {
+            'torso': np.array([0.5, 0.5, 0.3] + [0.5, 0.5, 0.001]),
+            'feet': np.array([4.] * 3 + [0.00001] * 3),  # (lin, ang)
+            'L_knee': np.array([2.] * 3 + [0.00001] * 3),
+            'R_knee': np.array([2.] * 3 + [0.00001] * 3),
+            'LH': np.array([4.] * 3 + [0.00001] * 3),
+            'RH': np.array([4.] * 3 + [0.00001] * 3)
+        }
     WBC_FINAL_FRAME_TRACKING_GAINS = {
-            'torso': np.array([3, 3.0, 1.5] + [0.5, 0.5, 0.1]),  # (lin, ang)  step over, on_balanced
-            # 'torso': np.array([5, 5.0, 5.0] + [1.0, 1.0, 1.]),  # (lin, ang)   step on
+            # 'torso': np.array([3, 3.0, 1.5] + [0.5, 0.5, 0.1]),  # (lin, ang)  step over, on_balanced
+            'torso': np.array([6, 6.0, 6.0] + [1.0, 1.0, 1.]),  # (lin, ang)   step on
             'feet': np.array([12.] * 3 + [6.5] * 3),  # (lin, ang)
             'L_knee': np.array([6.] * 3 + [0.00001] * 3),
             'R_knee': np.array([6.] * 3 + [0.00001] * 3),
@@ -79,19 +79,19 @@ class MultiContactDoorConfig(PlannerConfig):
         }
     WBC_WEIGHTED_COSTS = {
         #                 q_b_lin, q_b_ang, q_j, (v_b_lin, v_b_ang), v_j
-        'xReg': np.array([0] * 3 + [0.0] * 3 + [2.] * (N_V - 6) + [0.2] * 6 + [2.0] * (N_V - 6)), # step on
+        # 'xReg': np.array([0] * 3 + [0.0] * 3 + [2.] * (N_V - 6) + [0.2] * 6 + [2.0] * (N_V - 6)), # step on
         # 'xReg': np.array([0] * 3 + [3.0] * 3 + [2.0] * (N_V - 6) + [0.2] * 6 + [0.8] * (N_V - 6)),  # step over
-        # 'xReg': np.array([0] * 3 + [3.0] * 3 + [2.] * (N_V - 6) + [0.2] * 6 + [0.8] * (N_V - 6)), # step on balanced
-        # 'uReg': np.array([0.4] * N_U),
-        'uReg': np.array([0.5] * N_U),  # over, on
+        'xReg': np.array([0] * 3 + [0.0] * 3 + [2.] * (N_V - 6) + [0.2] * 6 + [8.0] * (N_V - 6)), # step on balanced
+        'uReg': np.array([0.4] * N_U),
+        # 'uReg': np.array([0.5] * N_U),  # over, on
     }
     WBC_PHASE_END_WEIGHTED_COSTS = {
-        # 'xReg': np.array([0] * 3 + [6.0] * 3 + [4.] * (N_V - 6) + [5.] * N_V),    # step on balanced
-        'xReg': np.array([0] * 3 + [30.0] * 3 + [5.] * (N_V - 6) + [40.] * N_V),    # step on balanced
+        # 'xReg': np.array([0] * 3 + [0.0] * 3 + [4.] * (N_V - 6) + [5.] * N_V),    # step on balanced
+        'xReg': np.array([0] * 3 + [30.0] * 3 + [5.] * (N_V - 6) + [30.] * N_V),    # step on balanced
     }
     WBC_FINAL_WEIGHTED_COSTS = {
         # 'xReg': np.array([0] * 3 + [30.0] * 3 + [5.] * (N_V - 6) + [20.] * N_V),    # step on, over
-        'xReg': np.array([0] * 3 + [30.0] * 3 + [5.] * (N_V - 6) + [40.] * N_V),    # step on balanced
+        'xReg': np.array([0] * 3 + [30.0] * 3 + [5.] * (N_V - 6) + [30.] * N_V),    # step on balanced
     }
 
 class MultiContactTiltedStairsConfig(PlannerConfig):
